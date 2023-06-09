4efd857150b2b65295c6c081941282d0d673c2d1
Ty <ty@malkincreative.com>
2023-06-09 00:08:24 -0500

    updating jsx

--- src/app/components/jobLayout.tsx
+++ src/app/components/jobLayout.tsx
@@ -3,69 +3,49 @@ import data from "../../json/data.json";
 
 const jobListings = data;
 
-interface isNewInterface {
-  newJob: boolean;
-}
-interface isFeaturedInterface {
-  Featured: boolean;
-}
-
-function IsNew({ newJob }: isNewInterface) {
-  if (!newJob) {
-    return null;
-  }
-  return <h2 className="bg-cyan-900 border-full p-2">{newJob}</h2>;
-}
-function IsFeatured({ Featured }: isFeaturedInterface) {
-  if (!Featured) {
-    return null;
-  }
-  return <h2 className="bg-black-900 border-full p-2">{Featured}</h2>;
-}
-
 export default function JobLayout() {
   return (
-    <ul>
+    <ul className="max-w-6xl m-auto py-8">
       {jobListings.map((jobListing) => (
-        <li className="mx-2 p-4 shadow-lg" key={jobListing.id}>
+        <li className="my-2 p-4 shadow-lg" key={jobListing.id}>
           <div className="flex justify-between">
             <div className="flex">
               <Image
                 src={jobListing.logo}
                 alt={jobListing.company}
                 width={50}
-                height={50}
+                height={20}
               />
               <div className="flex flex-col">
                 <div className="flex">
                   <h2 className="text-cyan-900">{jobListing.company}</h2>
-                  <IsNew newJob={jobListing.new} />
-                  <IsFeatured Featured={jobListing.featured} />
+                  {jobListing.new ? (
+                    <h2 className="bg-cyan-900 border-full p-2">New</h2>
+                  ) : null}
+                  {jobListing.featured ? (
+                    <h2 className="bg-black-900 border-full p-2">Featured</h2>
+                  ) : null}
                 </div>
                 <h3 className="text-black font-bold text-lg">
                   {jobListing.position}
                 </h3>
-                <div className="flex justify-between">
-                  <p className="text-slate-500 text-md">
-                    {jobListing.postedAt}
-                  </p>
-                  <p className="text-slate-500 text-md">
-                    {jobListing.contract}
-                  </p>
-                  <p className="text-slate-500 text-md">
-                    {jobListing.location}
-                  </p>
+                <div className="flex justify-between text-slate-500 text-md">
+                  <p>{jobListing.postedAt}</p>
+                  <p>{jobListing.contract}</p>
+                  <p>{jobListing.location}</p>
                 </div>
               </div>
             </div>
-            <div className="flex justify-between">
-              <h4 className="bg-cyan-500 text-cyan-900 border-1 border-cyan-900 p-2">
+            <div className="flex justify-between h-fit">
+              <h4 className="bg-cyan-500 text-cyan-900 border-1 border-cyan-900 p-2 mx-1">
                 {jobListing.role}
               </h4>
-              <h4 className="bg-cyan-500 text-cyan-900 border-1 border-cyan-900 p-2">
+              <h4 className="bg-cyan-500 text-cyan-900 border-1 border-cyan-900 p-2 mx-1">
                 {jobListing.position}
               </h4>
-              {jobListing.languages}
+              <h4 className="bg-cyan-500 text-cyan-900 border-1 border-cyan-900 p-2 mx-1">
+                {jobListing.languages}
+              </h4>
             </div>
           </div>
         </li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import JobLayout from "./components/jobLayout";
 
 export default function Home() {
   return (
-    <main className="min-h-full">
+    <main className="min-h-full bg-slate-100">
       <header className="min-h-8 bg-cyan-900">
         <Image
-          src="/bg-header-desktop"
+          src="/bg-header-desktop.svg"
           alt="header image"
           width={2000}
           height={150}

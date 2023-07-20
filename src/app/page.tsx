f6b3dcaacdfd2ab16161ce8f97ba80fba1ac1297
Ty <ty@malkincreative.com>
2023-07-20 10:42:56 -0500

    need to add filter functionality for limiting the jobs posted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,136 @@
+"use client";
+
 import Image from "next/image";
-import JobLayout from "./components/jobLayout";
+import data from "../json/data.json";
+import { useState } from "react";
+
+const jobListings = data;
+
+const filterColor = "bg-filter";
+const fontColor = "text-primary font-bold";
+const borderColor = "border-background";
+const buttonColor = "bg-primary";
+
+const bgColor = "bg-background";
 
 export default function Home() {
+  const [jobFilter, setJobFilter] = useState<string[]>([]);
+
+  function buildOutFilter() {
+    //run through filters and return job listings based on filters selected
+    //if no filters are selected return all jobs
+    return jobListings.map((jobListing) => (
+      <li
+        className="bg-white my-4 p-4 shadow-lg border-2 border-white rounded-md"
+        key={jobListing.id}
+      >
+        <div className="flex justify-between">
+          <div className="flex">
+            <Image
+              className="p-2"
+              src={jobListing.logo}
+              alt={jobListing.company}
+              width={90}
+              height={20}
+            />
+            <div className="flex flex-col">
+              <div className="flex items-center">
+                <h2 className={`${fontColor} font-black`}>
+                  {jobListing.company}
+                </h2>
+                {jobListing.new ? (
+                  <h2 className="bg-cyan-900 border-full py-1 px-2 text-white rounded-full mx-2">
+                    New
+                  </h2>
+                ) : null}
+                {jobListing.featured ? (
+                  <h2 className="bg-slate-900 border-full py-1 px-2 text-white rounded-full">
+                    Featured
+                  </h2>
+                ) : null}
+              </div>
+              <h3 className="text-black font-bold text-lg">
+                {jobListing.position}
+              </h3>
+              <div className="flex justify-between text-slate-500 text-md">
+                <p>{jobListing.postedAt}</p>
+                <p>{jobListing.contract}</p>
+                <p>{jobListing.location}</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between h-fit">
+            <button
+              className={`${fontColor} ${filterColor} border-1 border-cyan-900 p-2 mx-1`}
+              data-attribute={jobListing.role}
+              onClick={jobFilterEvent}
+            >
+              {jobListing.role}
+            </button>
+            <button
+              className={`${fontColor} ${filterColor} border-1 p-2 mx-1`}
+              data-attribute={jobListing.level}
+              onClick={jobFilterEvent}
+            >
+              {jobListing.level}
+            </button>
+            {jobListing.tools.map((tool, i) => (
+              <button
+                key={i}
+                className={`${fontColor} ${filterColor} border-1 p-2 mx-1`}
+                data-attribute={tool}
+                onClick={jobFilterEvent}
+              >
+                {tool}
+              </button>
+            ))}
+            {jobListing.languages.map((language, i) => (
+              <button
+                key={i}
+                className={`${fontColor} ${filterColor} border-1 p-2 mx-1`}
+                data-attribute={language}
+                onClick={jobFilterEvent}
+              >
+                {language}
+              </button>
+            ))}
+          </div>
+        </div>
+      </li>
+    ));
+  }
+  const jobFilterEvent = (event: any) => {
+    const jobAttribute = String(event.target.getAttribute("data-attribute"));
+
+    //if filter is already active prevent filter from attaching
+
+    const isFound = jobFilter.some((element) => {
+      if (element === jobAttribute) {
+        return true;
+      }
+      return false;
+    });
+
+    if (!isFound) {
+      setJobFilter([...jobFilter, jobAttribute]);
+    }
+  };
+
+  const removeJobFilter = (event: any) => {
+    var sibling = event.target.previousElementSibling;
+    const jobAttribute = String(sibling.getAttribute("data-attribute"));
+
+    setJobFilter(
+      jobFilter.filter((newFilterType) => newFilterType !== jobAttribute)
+    );
+  };
+  function resetJobFilter() {
+    setJobFilter(
+      jobFilter.filter((newFilterType) => newFilterType !== newFilterType)
+    );
+  }
   return (
-    <main className="min-h-full bg-slate-100">
+    <main className={`min-h-full ${bgColor}`}>
       <header className="min-h-8 bg-cyan-900">
         <Image
           src="/bg-header-desktop.svg"
@@ -12,7 +139,35 @@ export default function Home() {
           height={150}
         />
       </header>
-      <JobLayout />
+      <ul className="max-w-6xl m-auto py-8 relative">
+        {jobFilter.length > 0 ? (
+          <div className="absolute w-full px-4 py-8 -top-16 bg-white shadow-lg border-2 border-white rounded-md flex align-center flex justify-between">
+            <ul className="flex">
+              {jobFilter.map((filter, i) => (
+                <div className="flex px-2" key={i}>
+                  <li
+                    className={`${fontColor} ${filterColor} border-1 border-cyan-900 p-2 rounded-md`}
+                    data-attribute={filter}
+                  >
+                    {filter}
+                  </li>
+                  <button
+                    className={`h-full w-1/4 bg-cyan-900 border-full px-2 text-white rounded-sm ${buttonColor}`}
+                    onClick={removeJobFilter}
+                  >
+                    X
+                  </button>
+                </div>
+              ))}
+            </ul>
+            <button className={`${fontColor}`} onClick={resetJobFilter}>
+              Clear
+            </button>
+          </div>
+        ) : (
+          buildOutFilter()
+        )}
+      </ul>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      display: "League Spartan",
+    },
     extend: {
+      colors: {
+        primary: "hsl(180, 29%, 50%)",
+        background: "hsl(180, 52%, 96%)",
+        filter: "hsl(180, 31%, 95%)",
+        secondary: "hsl(180, 8%, 52%)",
+        tertiary: "hsl(180, 14%, 20%)",
+      },
+
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
